c4372e86276de6f4378489d0ff5cd3287ecd8b4b
Andrea Wu <lixianhyun@gmail.com>
2019-11-23 20:35:26 -0800

    add game.css

--- game.js
+++ game.js
@@ -1,59 +1,58 @@
-
-alert("Are you ready?");
-
-let selection = prompt("rock, paper or scissors?!");
-
-let playerSelection = selection;
-
-let choice = ["rock", "paper", "scissors"];
-
-function computerSelection(choice){
-    
-    let rand = Math.floor(Math.random() * 3) + 0;
-    return (computerSelection[rand]);
-}
+const playerSelection = document.getElementsByTagName("input");
+let input = " ";
 
 let playerScore = 0;
 let computerScore = 0;
 
+function computerPlay() {
+    let randomNumber = Math.random();
+    if (randomNumber < 0.33) {
+      return 'rock';
+    } else if (randomNumber > 0.66) {
+      return 'paper';
+    } else {
+      return 'scissors';
+    }
+  }
+
+  let computerSelection = computerPlay;
+
 function playRound(playerSelection, computerSelection) {
-    while (playerScore <= 5) {
-        if(playerSelection === "rock" && computerSelection === "paper"){
+
+    if(playerSelection === "rock" && computerSelection === "paper"){
         computerScore++;
         return "You loose!";
     }
 
-        else if(playerSelection === "rock" && computerSelection === "scissors"){
-            playerScore++;
-            return "You win!";
-        }
+    else if(playerSelection === "rock" && computerSelection === "scissors"){
+        playerScore++;
+        return "You win!";
+    }
 
-        if(playerSelection === "scissors" && computerSelection === "paper"){
-            playerScore++;
-            return "You win!";
-        }
+    else if(playerSelection === "scissors" && computerSelection === "paper"){
+        playerScore++;
+        return "You win!";
+    }
 
-        else if(playerSelection === "scissors" && computerSelection === "rock"){
-            computerScore++;
-            return "You loose!";
-        }
+    else if(playerSelection === "scissors" && computerSelection === "rock"){
+        computerScore++;
+        return "You loose!";
+    }
 
-        if(playerSelection === "paper" && computerSelection === "rock"){
-            playerScore++;
-            return "You win!";
-        }
+    else if(playerSelection === "paper" && computerSelection === "rock"){
+        playerScore++;
+        return "You win!";
+    }
 
-        else if(playerSelection === "paper" && computerSelection === "scissors"){
-            computerScore++;
-            return "You loose!";
-        }
+    else if(playerSelection === "paper" && computerSelection === "scissors"){
+        computerScore++;
+        return "You loose!";
+    }
 
-        else if(playerSelection === computerSelection){
-            return "It's tie";
-        }
+    else if(playerSelection === computerSelection){
+        return "It's a tie";
     }
     
-	
 }
 
 
